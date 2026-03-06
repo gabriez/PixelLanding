a50cf32c2ab9136bd0daa4558280bfd0c0568825
gabotdev <gabrieltorres2014@gmail.com>
2026-03-06 16:35:10 -0400

    PXL-01 | feat: added link to calendly

--- src/components/ScheduleButton.tsx
+++ src/components/ScheduleButton.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import Link from "next/link";
 
 export const ScheduleButton = () => {
   return (
-    <button className="schedule-assesory font-bold cursor-pointer max-sm:w-full 2xl:text-lg px-6 py-4 2xl:px-7 2xl:py-5 mx-auto block">
+    <Link
+      href="https://calendly.com/gabrieltdeveloper2014/30min"
+      target="_blank"
+      className="schedule-assesory font-bold cursor-pointer max-sm:w-full 2xl:text-lg px-6 py-4 2xl:px-7 2xl:py-5 mx-auto block"
+    >
       Agenda una asesoría gratuita
-    </button>
+    </Link>
   );
 };
