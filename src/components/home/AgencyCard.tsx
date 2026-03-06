ca27d18c898a97cd4f1a6e638328233717ae6455
DeiNas77 <deinas.44Remastered@gmail.com>
2026-03-06 09:53:33 -0500

    PXL-01 | feat: change color in agencycard icon

--- src/components/home/AgencyCard.tsx
+++ src/components/home/AgencyCard.tsx
@@ -7,7 +7,10 @@ export const AgencyCard = ({ agencyCard }: AgencyCardRender) => (
     className="bg-[#181B25] p-5 flex items-center gap-4 rounded-2xl border border-(--primary-color)/50  hover:border-(--primary-color)/40 transition-colors"
   >
     <div className="bg-[#241F3B] p-3 rounded-2xl shrink-0 w-fit h-fit">
-      <IconView icon={agencyCard.icon} className="w-5 h-5 text-purple-500" />
+      <IconView
+        icon={agencyCard.icon}
+        className="w-5 h-5 text-(--primary-color)"
+      />
     </div>
     <div>
       <p className="text-white  font-(--plus-jakarta-sans)">
